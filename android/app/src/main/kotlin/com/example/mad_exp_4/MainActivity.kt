a8218f7ceb5e264e8958b1112bb39b1599e89bfd
Omjannu <89771105+Om-jannu@users.noreply.github.com>
2023-04-27 01:02:43 +0530

    initial commit

---
+++ android/app/src/main/kotlin/com/example/mad_exp_4/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.mad_exp_4
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import './subjectFaculty.dart';
+void main() {
+  runApp(MaterialApp(
+    home: MyList(),
+  ));
+}
+
+class MyList extends StatefulWidget {
+  const MyList({Key? key}) : super(key: key);
+
+  @override
+  State<MyList> createState() => _MyListState();
+}
+
+class _MyListState extends State<MyList> {
+
+  List<SubjectFaculty> subjects =[
+    SubjectFaculty(sub:"AI_DS",teacher:"Prof. Prasad Padalkar"),
+    SubjectFaculty(sub:"MAD_PWA",teacher:"Prof. Nilesh Ghavate"),
+    SubjectFaculty(sub:"DMBI",teacher:"Mrs Aruna"),
+    SubjectFaculty(sub:"AIDS",teacher:"Mrs Sunantha"),
+    SubjectFaculty(sub:"EH",teacher:"Mrs Jhanvi"),
+    SubjectFaculty(sub:"AIML",teacher:"Prof. Uday"),
+    SubjectFaculty(sub:"WT",teacher:"Prof. Tayyabali"),
+  ];
+
+Widget subjectTemplate(sub){
+  return Card(
+
+    margin: EdgeInsets.fromLTRB(10.0,10.0,10.0,10.0),
+    child: Padding(
+      padding: const EdgeInsets.all(12.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children:<Widget> [
+          Text(sub.subject),
+          SizedBox(height: 10.0,),
+          Text(sub.faculty),
+        ],
+      ),
+    )
+
+  );
+}
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey,
+      appBar: AppBar(
+        title: Text ("Subjects"),
+        centerTitle: true,
+      ),
+      body: Column(
+        children: subjects.map((sub) => subjectTemplate(sub)).toList(),
+      ),
+
+    );
+  }
+}
+
